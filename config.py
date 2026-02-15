0b69a219764ae3ec0e10ba1f75847de788e2935f
Theguardians58 <125973002+Theguardians58@users.noreply.github.com>
2026-02-15 15:48:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-
 import os
 
 # Login feature, if you want then True , if you don't want then False
@@ -29,8 +28,8 @@ CHANNEL_ID = os.environ.get("CHANNEL_ID", "")
 
 # Your Mongodb Database Url
 # Warning - Give Db uri in deploy server environment variable, don't give in repo.
-DB_URI = os.environ.get("DB_URI", "") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
-DB_NAME = os.environ.get("DB_NAME", "vjsavecontentbot")
+DB_URI = os.environ.get("DB_URI", "mongodb+srv://theguardianhacker:Alter$01@cluster0.gguvt.mongodb.net/?appName=Cluster0") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
+DB_NAME = os.environ.get("DB_NAME", "theguardianhacker")
 
 # Increase time as much as possible to avoid floodwait, spamming and tg account ban issues.
 WAITING_TIME = int(os.environ.get("WAITING_TIME", "10")) # time in seconds
